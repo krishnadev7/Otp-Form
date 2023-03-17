fe4f9804c01e39d152cf128e7c48a9e8767aa339
krishnadev7 <krishnadevv12@gmail.com>
2023-03-17 22:49:47 +0530

    formik added

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,73 @@
 import { useEffect, useRef, useState } from 'react';
+import { Formik, useFormik } from 'formik';
 import './App.css';
 
+const validate = (values) => {
+  const errors = {}
+  if (values.otp.some(data => data === '')) {
+    errors.otp = 'This field is required';
+  }
+  return errors;
+}
+
 function App() {
-  const inputRef = useRef({});
-  const [otp, setOtp] = useState({
-    digitOne: '',
-    digitTwo: '',
-    digitThree: '',
-    digitFour: '',
-    digitFive: '',
-    digitSix: '',
+  const formik = useFormik({
+    initialValues: {
+      otp: Array.from({length: 6}).fill("")
+    },
+    validate,
+    onSubmit: (values) => {
+      console.log(values);
+    }
   });
+
+  // console.log(formik.values);
+
+  const inputRef = useRef({});
+
+  // const [otp, setOtp] = useState({
+  //   digitOne: '',
+  //   digitTwo: '',
+  //   digitThree: '',
+  //   digitFour: '',
+  //   digitFive: '',
+  //   digitSix: '',
+  // });
 
   useEffect(() => {
     inputRef.current[0].focus();
 
-    inputRef.current[0].addEventListener("paste",pasteText);
+    inputRef.current[0].addEventListener('paste', pasteText);
 
-    return () => inputRef.current[0].removeEventListener("paste",pasteText);
-
-  },[])
+    return () => inputRef.current[0].removeEventListener('paste', pasteText);
+  }, []);
 
   function pasteText(e) {
-      const pastedText = e.clipboardData.getData("text");
-      const fieldValue = {}
-      Object.keys(otp).forEach((key,index) => {
-          fieldValue[key] = pastedText[index];
-      })
-      setOtp(fieldValue)
-      inputRef.current[5].focus();
+    const pastedText = e.clipboardData.getData('text');
+    const fieldValue = {};
+    Object.keys(otp).forEach((key, index) => {
+      fieldValue[key] = pastedText[index];
+    });
+    setOtp(fieldValue);
+    inputRef.current[5].focus();
   }
 
   const handleChange = (e, index) => {
-    const { name, value } = e.target;
-    if(/[a-z]/gi.test(value)) return;
-    setOtp(prev => ({
+    const { value } = e.target;
+    if (/[a-z]/gi.test(value)) return;
+
+    const currentOtp = [...formik.values.otp]
+    currentOtp[index] = value.slice(-1);
+
+    formik.setValues((prev) => ({
       ...prev,
-      [name]: value.slice(-1),
-    }));
+      otp: currentOtp
+    }))
+
+    // setOtp(prev => ({
+    //   ...prev,
+    //   [name]: value.slice(-1),
+    // }));
 
     if (value && index < 5) inputRef.current[index + 1].focus();
   };
@@ -51,13 +81,13 @@ function App() {
   };
 
   const renderInput = () => {
-    return Object.keys(otp).map((keys, index) => (
+    return formik.values.otp.map((values, index) => (
       <input
         key={index}
         ref={element => (inputRef.current[index] = element)}
         className='w-16 h-12 rounded-md text-center mr-3 text-xl'
-        value={otp[keys]}
-        name={keys}
+        value={values}
+        name={index}
         onChange={e => handleChange(e, index)}
         onKeyUp={e => handleBackSpace(e, index)}
       />
@@ -67,8 +97,13 @@ function App() {
   return (
     <form action=''>
       <h3 className='text-3xl mb-8'>Please fill in the form</h3>
-      <div>{renderInput()}</div>
-      <button className='mt-4 w-32 border border-solid bg-[#3b3b3b] rounded hover:bg-[#252525] hover:border-[#3b3b3b]'>
+      <Formik>
+        <div>{renderInput()}</div>
+      </Formik>
+      {formik.errors.otp && <p>Please fill the fields</p>}
+      <button type='button' className='mt-4 w-32 border border-solid bg-[#3b3b3b] rounded hover:bg-[#252525] hover:border-[#3b3b3b]'
+        onClick={formik.handleSubmit}
+      >
         Submit
       </button>
     </form>
